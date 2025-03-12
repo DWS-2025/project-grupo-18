bf4f01f9478d44a252d2dac9f3d103aeb58818aa
dmartinm2023 <d.martinm.2023@alumnos.urjc.es>
2025-03-12 18:26:58 +0100

    Sin conflictos, falta el HTML

--- src/main/java/es/grupo18/jobmatcher/controller/ProfileController.java
+++ src/main/java/es/grupo18/jobmatcher/controller/ProfileController.java
@@ -1,27 +1,33 @@
 package es.grupo18.jobmatcher.controller;
 
+import es.grupo18.jobmatcher.model.Company;
 import es.grupo18.jobmatcher.model.User;
+import es.grupo18.jobmatcher.service.CompanyService;
 import es.grupo18.jobmatcher.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 @Controller
 public class ProfileController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private CompanyService companyService;
+
+    private static final Path IMAGES_FOLDER = Paths.get("src/main/resources/static/img");
 
     @GetMapping("/profile")
     public String showProfile(Model model) {
@@ -31,14 +37,14 @@ public class ProfileController {
 
     @PostMapping("/profile/upload_image")
     public String uploadImage(@RequestParam("image") MultipartFile image) throws IOException {
-        User user = userService.getUser();
+        User user = userService.getLoggedUser();
 
         if (!image.isEmpty()) {
             // Ensure the images folder exists
             Files.createDirectories(IMAGES_FOLDER);
 
             // Unique file name for the image
-            String fileName = "profile_" + user.getAccountId() + ".jpg";
+            String fileName = "profile_" + user.getId() + ".jpg";
             Path imagePath = IMAGES_FOLDER.resolve(fileName);
 
             // Saves the image in the images folder
@@ -52,44 +58,9 @@ public class ProfileController {
         return "redirect:/profile";
     }
 
-    @GetMapping("/profile/edit") // Shows the profile editor
-    public String editProfile(Model model) {
-        User user = userService.getUser();
-        model.addAttribute("name", user.getName());
-        model.addAttribute("email", user.getEmail());
-        model.addAttribute("phone", user.getPhone());
-        model.addAttribute("location", user.getLocation());
-        model.addAttribute("bio", user.getBio());
-        return "profileEditor";
-    }
-
-    @PostMapping("/profile/edit") // Saves the profile changes
-    public String saveProfile(@RequestParam String name, @RequestParam String email, @RequestParam String phone,
-            @RequestParam String location, @RequestParam String bio) {
-        userService.updateUserProfile(name, email, phone, location, bio);
-        return "redirect:/profile";
-    }
-
-    @GetMapping("/profile/form") // Shows the form to edit the user's profile (CV, skills and experiences)
-    public String editProfileInfo(Model model) {
-        User user = userService.getUser();
-        model.addAttribute("user", user);
-        model.addAttribute("studies", String.join(", ", user.getDegrees()));
-        model.addAttribute("skills", String.join(", ", user.getSkills()));
-        model.addAttribute("experience", user.getExperience());
-        return "form";
-    }
-
-    @PostMapping("/profile/form") // Saves the profile info changes
-    public String saveProfileInfo(@RequestParam String studies, @RequestParam String skills,
-            @RequestParam Integer experience) {
-        userService.updateUserDetails(studies, skills, experience);
-        return "redirect:/profile";
-    }
-
     @PostMapping("/profile/reset_image")
     public ResponseEntity<Void> resetProfileImage() throws IOException {
-        User user = userService.getUser();
+        User user = userService.getLoggedUser();
 
         Path userImagePath = Paths.get("src/main/resources/static" + user.getImagePath());
 
@@ -104,4 +75,69 @@ public class ProfileController {
         return ResponseEntity.ok().build();
     }
 
+    @PostMapping("/profile/edit")
+    public String updateProfile(@RequestParam String name, @RequestParam String email, @RequestParam String password,
+            @RequestParam String phone,
+            @RequestParam String location, @RequestParam String bio, @RequestParam int experience) {
+        userService.update(name, email, password, phone, location, bio, experience);
+        return "redirect:/profile";
+    }
+
+    @GetMapping("/companies")
+    public String getCompanies(Model model) {
+        model.addAttribute("companies", companyService.findAll());
+        return "companies";
+    }
+
+    @PostMapping("/companies/new")
+    public String newCompany(Model model, Company company) {
+        companyService.save(company);
+        return "redirect:/companies";
+    }
+
+    @GetMapping("/companies/{id}")
+    public String getCompany(Model model, @PathVariable long id) {
+        Optional<Company> company = Optional.ofNullable(companyService.findById(id));
+        if (company.isPresent()) {
+            model.addAttribute("company", company.get());
+            return "show_company";
+        } else {
+            return "company_not_found";
+        }
+    }
+
+    @GetMapping("/companies/{id}/edit")
+    public String editCompany(Model model, @PathVariable long id) {
+        Optional<Company> company = Optional.ofNullable(companyService.findById(id));
+        if (company.isPresent()) {
+            model.addAttribute("company", company.get());
+            return "edit_company";
+        } else {
+            return "company_not_found";
+        }
+    }
+
+    @PostMapping("/companies/{id}/edit")
+    public String updateCompany(Model model, @PathVariable long id, Company updatedCompany) {
+        Optional<Company> op = Optional.ofNullable(companyService.findById(id));
+        if (op.isPresent()) {
+            Company oldCompany = op.get();
+            companyService.update(oldCompany, updatedCompany);
+            return "redirect:/companies/" + id;
+        } else {
+            return "company_not_found";
+        }
+    }
+
+    @PostMapping("/companies/{id}/delete")
+    public String deleteCompany(@PathVariable long id) {
+        Optional<Company> op = Optional.ofNullable(companyService.findById(id));
+        if (op.isPresent()) {
+            companyService.delete(op.get());
+            return "redirect:/companies";
+        } else {
+            return "company_not_found";
+        }
+    }
+    
 }
