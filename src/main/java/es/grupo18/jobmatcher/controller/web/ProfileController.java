2c28968ed80010bf31b1de983b3b19359c0fa97b
CarlosMarronBenito <160424096+CarlosMarronBenito@users.noreply.github.com>
2025-05-09 13:35:48 +0200

    image delete fix

--- src/main/java/es/grupo18/jobmatcher/controller/web/ProfileController.java
+++ src/main/java/es/grupo18/jobmatcher/controller/web/ProfileController.java
@@ -1,8 +1,6 @@
 package es.grupo18.jobmatcher.controller.web;
 
-import es.grupo18.jobmatcher.dto.UserDTO;
-import es.grupo18.jobmatcher.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -10,10 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import es.grupo18.jobmatcher.dto.UserDTO;
+import es.grupo18.jobmatcher.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class ProfileController {
