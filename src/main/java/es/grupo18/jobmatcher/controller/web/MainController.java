998852f31aeb548b4b755b3691c9fee175bea6c4
CarlosMarronBenito <160424096+CarlosMarronBenito@users.noreply.github.com>
2025-05-15 10:49:37 +0200

    different main per rol

--- src/main/java/es/grupo18/jobmatcher/controller/web/MainController.java
+++ src/main/java/es/grupo18/jobmatcher/controller/web/MainController.java
@@ -1,14 +1,28 @@
 package es.grupo18.jobmatcher.controller.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/main") // Shows the main page
-    public String showMainPage() {
+    @GetMapping("/main") 
+    public String showMainPage(Model model, Authentication authentication) {
+        boolean isAdmin = false;
+
+        if (authentication != null) {
+            for (GrantedAuthority auth : authentication.getAuthorities()) {
+                if ("ROLE_ADMIN".equals(auth.getAuthority())) {
+                    isAdmin = true;
+                    break;
+                }
+            }
+        }
+
+        model.addAttribute("isAdmin", isAdmin);
         return "main";
     }
-
 }
